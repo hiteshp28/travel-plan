bb0730f2e88f396ccd393dcfeb47865bc1483163
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-02-18 14:37:14 +0530

    Added Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Analytics>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
       <BrowserRouter>
           <App/>
       </BrowserRouter>
     </GoogleOAuthProvider>
+    </Analytics>
   </React.StrictMode>
 )
